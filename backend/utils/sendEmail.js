f9378c93c9b7a6eaef3f99226ad8e5af84ec8a7b
Ankit Raj <ankitrajqqq@gmail.com>
2026-09-07 00:02:37 +0530

    Fix SMTP IPv4 connection

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,8 +3,9 @@ import nodemailer from 'nodemailer';
 export const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    port: Number(process.env.EMAIL_PORT),
     secure: false,
+    family: 4,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
